963cab65fb7c986cb34217bb91cfa107c046b6ce
junghye01 <tweetiijh@naver.com>
2023-07-20 22:46:50 +0000

    Refactor text target tensor code

--- module/pixt_basemodule.py
+++ module/pixt_basemodule.py
@@ -40,25 +40,27 @@ class BaselineLitModule(pl.LightningModule):
         text_input_ko_list = []
         for tags_ko in text_ko:  # batch size 만큼 iteration
             for tag_ko in tags_ko:  # data sample 당 tag 개수만큼 iteration
-                text_input_ko_list.append(tag_ko)
-        text_input_ko_list = list(set(text_input_ko_list))
+                text_input_ko_list.append((tag_ko,1))
+        
+        # 중복된 라벨 제거
+        unique_text_input_ko_list=list(set(text_input_ko_list))
+        
 
         # false label with negative sampling 인 tag 담기
-        num_true_labels=len(text_input_ko_list)
-        num_false_labels=self._max_length-num_true_labels
+        num_false_labels=self._max_length-len(unique_text_input_ko_list)
 
-        available_false_labels=list(set(self._tags_ko_all_list)-set(text_input_ko_list))
-        sampled_false_labels=random.sample(self._tags_ko_all_list,num_false_labels)
-        text_input_ko_list.extend(sampled_false_labels)
+        available_false_labels=list(set(self._tags_ko_all_list)-set([tag_ko for tag_ko,_ in unique_text_input_ko_list]))
+
+        sampled_false_labels=random.sample(available_false_labels,num_false_labels)
+        unique_text_input_ko_list.extend([(random_sample,0) for random_sample in sampled_false_labels])
+
 
         # 한국어(text_input_ko_list)에서 영어(text_input_en_list)로 번역하기
-        text_input_en_list = [
-            self._tags_en_all_list[self._tags_ko_all_list.index(tag_ko)]
-            for tag_ko in text_input_ko_list
-        ]
-        # tokenize 수행 및 tensor 변환
-        text_tensor = torch.cat([clip.tokenize(f"a photo of a {c}") for c in text_input_en_list])
+        text_input_en_list=[self._tags_en_all_list[self._tags_ko_all_list.index(tag_ko)] for tag_ko,_ in unique_text_input_ko_list]
+        text_tensor=torch.cat([clip.tokenize(f"a photo of a {c}") for c in text_input_en_list])
 
+        
+        # target tensor
         # 한국어(text_ko)에서 영어(text_en)로 번역하기
         text_en = []
         for tags_ko in text_ko:  # batch size 만큼 iteration
@@ -70,12 +72,12 @@ class BaselineLitModule(pl.LightningModule):
         # target tensor 생성
         target_tensor_list = []
         for tags_en in text_en:
-            target_tensor = torch.zeros_like(torch.empty(self._max_length))
-            for tag_en in tags_en:
-                if tag_en in text_input_en_list:
-                    target_tensor[text_input_en_list.index(tag_en)] = 1
+            target_tensor=torch.zeros(self._max_length,dtype=torch.float)
+            indices=[text_input_en_list.index(tag_en) for tag_en in tags_en if tag_en in text_input_en_list]
+            target_tensor[indices]=1
             target_tensor_list.append(target_tensor)
         target_tesnor = torch.stack(target_tensor_list, dim=0)
+      
 
         return text_tensor, target_tesnor
 
@@ -118,6 +120,7 @@ class BaselineLitModule(pl.LightningModule):
     def validation_step(self, batch, batch_idx) -> dict[str, Any]:
         image, text, target = self._parse_batch(batch)
         logits_per_image, logits_per_text = self._clip_model(image, text)
+        #target=target.view(-1)
         loss = self._base_loss_func(logits_per_image, logits_per_text, target)
 
         self.log("valid\ce_loss", loss, on_step=False, on_epoch=True, batch_size=image.shape[0])
