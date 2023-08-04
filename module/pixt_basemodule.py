d72984d6dc99c00f3880445ec839573b6b5b37d2
junghye01 <tweetiijh@naver.com>
2023-08-04 02:08:30 +0000

    ADD 검증 모듈

--- module/pixt_basemodule.py
+++ module/pixt_basemodule.py
@@ -5,6 +5,8 @@ import torch
 import torch.nn as nn
 import json
 import os
+from pathlib import Path
+from datetime import datetime
 
 
 class BaselineLitModule(pl.LightningModule):
@@ -26,10 +28,13 @@ class BaselineLitModule(pl.LightningModule):
         self._optim = optim
         self._lr = lr
         self.automatic_optimization = False
+        self.save_dir=save_dir
         self._test_log_dict = {}
+      
+    
         self.classes_ko_dir=classes_ko_dir
         self.classes_en_dir=classes_en_dir
-
+        
         
         #load tags
         if classes_ko_dir is not None and classes_en_dir is not None:
@@ -60,22 +65,43 @@ class BaselineLitModule(pl.LightningModule):
             weight_decay=0.2,
         )
 
-    def training_step(self, batch, batch_idx) -> None:
-        optim = self.optimizers()
-        image, text, target, text_en, text_input = self._parse_batch(batch)
-
-        image_features = self._clip_model.encode_image(image)
-        text_features = self._clip_model.encode_text(text)
+    def forward(self,image,text):
+        image_features=self._clip_model.encode_image(image)
+        text_features=self._clip_model.encode_text(text)
 
         image_features = image_features / image_features.norm(dim=-1, keepdim=True)
         text_features = text_features / text_features.norm(dim=-1, keepdim=True)
 
-        image_features=image_features.to('cuda').half()
-        text_features=text_features.to('cuda').half()
+        image_features = image_features.to('cuda').half()
+        text_features = text_features.to('cuda').half()
 
+        return image_features,text_features
+
+    def training_step(self, batch, batch_idx) -> None:
+       
+
+        optim = self.optimizers()
+        image, text, target, text_en, text_input = self._parse_batch(batch)
+
+        image_features=self._clip_model.encode_image(image)
+        text_features=self._clip_model.encode_text(text)
+
+        image_features = image_features / image_features.norm(dim=-1, keepdim=True)
+        text_features = text_features / text_features.norm(dim=-1, keepdim=True)
+
+        image_features = image_features.to('cuda').half()
+        text_features = text_features.to('cuda').half()
+
+        
+        
+        target=target.to('cuda').half()
+
+
+        # cross-entropy loss
+        
         similarity = image_features @ text_features.T
 
-        target=target.to('cuda').half()
+       
         
         loss = self._base_loss_func(similarity, target)
         acc = self._accuracy(similarity, text_en, text_input)
@@ -86,11 +112,16 @@ class BaselineLitModule(pl.LightningModule):
 
         self.log("train/mlsm_loss", loss, on_step=True, on_epoch=True, batch_size=image.shape[0])
         self.log("train/accuracy", acc, on_step=True, on_epoch=True, batch_size=image.shape[0])
+       # wandb.log({
+            #"train/mlsm_loss":loss,
+           # "train/accuracy":acc,
+        #})
 
 
 
     def validation_step(self, batch, batch_idx) -> dict[str, Any]:
         image, text, target,text_en,text_input = self._parse_batch(batch)
+        
         
         image_features = self._clip_model.encode_image(image)
         text_features = self._clip_model.encode_text(text)
@@ -99,24 +130,29 @@ class BaselineLitModule(pl.LightningModule):
 
         image_features=image_features.to('cuda').half()
         text_features=text_features.to('cuda').half()
-        
+      
+
         similarity = image_features @ text_features.T
 
+       
 
         target=target.to('cuda').half()
        
 
         loss = self._base_loss_func(similarity, target)
+        
         acc = self._accuracy(similarity, text_en, text_input)
 
         self.log("valid/mlsm_loss", loss, on_step=True, on_epoch=True, batch_size=image.shape[0])
         self.log("valid/accuracy", acc, on_step=True, on_epoch=True, batch_size=image.shape[0])
+       
 
     def test_step(self, batch, batch_idx) -> None:
         image_tensor = batch.get("image_tensor", None)
         classes_list = list(set([tag_en.lower() for tag_en in self._tags_en_all_list]))
         text_tensor = torch.cat([clip.tokenize(f"a photo of a {c}") for c in classes_list])
 
+        #print(len(classes_list))
         # lower case와 original case 맵핑
         tag_mapping={tag_en.lower():tag_en for tag_en in self._tags_en_all_list}
         
@@ -132,17 +168,20 @@ class BaselineLitModule(pl.LightningModule):
         text_features=text_features.to('cuda').half()
 
 
-        #similarity = (100.0 * image_features @ text_features.T).softmax(dim=-1)
-        similarity=image_features @ text_features.T
+        similarity = (100.0 * image_features @ text_features.T).softmax(dim=-1)
+        #similarity=image_features @ text_features.T
         values, indices = similarity[0].topk(10)
+        
 
         # batch에 있는 이미지 파일명
 
         batch_image_filenames = batch.get("image_filename", None)
 
-        print(batch_image_filenames, batch_idx)
+        #print(batch_image_filenames, batch_idx)
         image_filename = batch_image_filenames[0]
 
+        print('파일명',image_filename)
+        print(values)
         image_prediction_dict = {}
 
         for value, index in zip(values, indices):
@@ -150,15 +189,25 @@ class BaselineLitModule(pl.LightningModule):
             eng_tag=tag_mapping[eng_tag_lower]
             ko_tag=self._tags_ko_all_list[self._tags_en_all_list.index(eng_tag)]
 
-            #print('뭐지',eng_tag,ko_tag)
             formatted_tag=f"{eng_tag_lower}({ko_tag})"
             image_prediction_dict[formatted_tag] = round(value.item(),6)
       
-
+        print('예측 결과',image_prediction_dict)
         self._test_log_dict[image_filename] = image_prediction_dict
-        print("result", self._test_log_dict)
+        #print("result", self._test_log_dict)
 
     def on_test_end(self) -> None:
         with open(os.path.join(self.save_dir,'test_results.json'),"w",encoding='utf-8') as f:
-            json.dump(self._test_log_dict,f,indent='\t')
-            print(f'{self.save_dir}에 저장되었습니다')
+            json.dump(self._test_log_dict,f,indent='\t',ensure_ascii=False)
+            print(f'{self.save_dir}에 test log가 저장되었습니다')
+
+"""
+    def on_epoch_end(self) -> None:
+        avg_loss = self.trainer.callback_metrics['valid/mlsm_loss']
+        avg_acc = self.trainer.callback_metrics['valid/accuracy']
+
+        with open(self.progress_log_path, "a", encoding='utf-8') as f:
+            result_str = f"Validation - average loss: {avg_loss:.3f}, average acc: {avg_acc:.3f}\n"
+            f.write(result_str)
+
+"""
---
+++ testmodule/pixt_testmodule.py
@@ -0,0 +1,160 @@
+import sys
+
+sys.path.append("/home/irteam/junghye-dcloud-dir/Pixt/code/Pixt")
+
+
+import os
+import glob
+import yaml
+import clip
+import torch
+import torch.nn as nn
+
+
+from loss import BaseLoss
+from metrics import Accuracy
+from module import BaselineLitModule
+
+from lightning.pytorch.callbacks import ModelCheckpoint
+
+from PIL import Image
+import matplotlib.pyplot as plt
+from typing import List
+import pandas as pd
+
+
+class ModelLoader(nn.Module):
+    def __init__(self,root_dir:str):
+        super(ModelLoader,self).__init__()
+        self.root_dir=root_dir
+        self.device=torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+        self.model,_=clip.load("RN50", device=self.device)
+        self.classes_ko_dir=None
+        self.classes_en_dir=None
+
+    def forward(x):
+        return x
+
+    def load_model(self):
+        config_path=os.path.join(self.root_dir,'config.yaml')
+        ckpt_path=glob.glob(os.path.join(self.root_dir,'*.ckpt'))[0]
+        cfg=yaml.load(open(config_path,'r'),Loader=yaml.FullLoader)
+
+        self.classes_ko_dir=cfg['module']['classes_ko_dir']
+        self.classes_en_dir=cfg['module']['classes_en_dir']
+
+        self.base_loss=BaseLoss(base_loss_weight=cfg['loss']['ce_loss_weight'])
+
+        self.accuracy=Accuracy()
+
+        lit_module = BaselineLitModule(
+            clip_model=self.model,
+            base_loss_func=self.base_loss,
+            accuracy=self.accuracy,
+            optim=torch.optim.Adam,
+            lr=cfg["module"]["lr"],
+            save_dir=os.path.join(cfg["logger"]["save_root"], cfg["logger"]["log_dirname"]),
+            classes_ko_dir=self.classes_ko_dir,
+            classes_en_dir=self.classes_en_dir,
+        )
+        lit_module.load_state_dict(torch.load(ckpt_path)["state_dict"])
+
+        return lit_module
+
+    def model_forward(self,image,text):
+        return self.load_model().forward(image,text)
+
+
+class ModelTester(nn.Module):
+    def __init__(self,model,image_transform,image_paths:List[str],ann_dir:str):
+        super(ModelTester,self).__init__()
+        self.model=model
+        self.image_transform=image_transform
+        self.test_log_dict={}
+        self._tags_en_all_list=self._get_tags_all_list(self.model.classes_en_dir)
+        self._tags_ko_all_list=self._get_tags_all_list(self.model.classes_ko_dir)
+        self.image_paths=image_paths
+        self.ann_dir=ann_dir
+   
+
+    def forward(self,image,text):
+        return self.model(image,text)
+
+    def _get_tags_all_list(self, classes_dir: str) -> list:
+        return torch.load(classes_dir)
+
+    def show_image(self):
+        num_images = len(self.image_paths)
+        num_cols = 4  
+        num_rows = (num_images + num_cols - 1) // num_cols
+
+        fig, axs = plt.subplots(num_rows, num_cols, figsize=(15, 5 * num_rows))
+        axs = axs.flatten()
+
+        for i, image_path in enumerate(self.image_paths):
+            img = Image.open(image_path).convert("RGB")
+            axs[i].imshow(img)
+            axs[i].axis("off")
+
+       
+        for i in range(num_images, num_cols * num_rows):
+            axs[i].axis("off")
+
+        plt.tight_layout()
+        plt.show()
+    
+    def test_step(self):
+        # image -> image tensor
+        images=[self.image_transform(Image.open(image_path).convert('RGB')).float() for image_path in self.image_paths ]
+        images=torch.stack(images).to('cuda')
+
+        # text -> text token
+        classes_list = list(set([tag_en for tag_en in self._tags_en_all_list]))
+        text_tensor = torch.cat([clip.tokenize(f"a photo of a {c}") for c in classes_list])
+        text_tensor=text_tensor.to('cuda')
+
+        true_labels = self._get_true_labels(self.image_paths, self.ann_dir)
+
+        # prediction
+        with torch.no_grad():
+            image_features,text_features=self.model(images,text_tensor)
+
+            similarity = (100.0 * image_features @ text_features.T).softmax(dim=-1)
+
+            values,indices=similarity[0].topk(10)
+
+            for i in range(len(images)):
+                image_filename=os.path.basename(self.image_paths[i])
+                image_prediction_dict={}
+                for value,index in zip(values,indices):
+                    eng_tag=classes_list[index]
+                    ko_tag=self._tags_ko_all_list[self._tags_en_all_list.index(eng_tag)]
+
+                    formatted_tag = f"{eng_tag}({ko_tag})"
+                    image_prediction_dict[formatted_tag] = round(value.item(), 6)
+
+                self.test_log_dict[image_filename] = {
+                "prediction": image_prediction_dict,
+                "true_label": true_labels[i],
+            }
+
+        return self.test_log_dict
+
+    def _get_true_labels(self,image_paths,ann_dir):
+        ann=pd.read_csv(ann_dir)
+        true_labels=[]
+        for i, image_path in enumerate(image_paths):
+            dataset,file_name=image_path.split('/')[-2],image_path.split('/')[-1]
+
+            filtered_row=ann[(ann['dataset']==dataset) & (ann['image_number']==int(file_name[0]))]
+
+            true_label=eval(filtered_row['tags_ko'].iloc[0])
+            true_label_modified=[]
+            for label in true_label:
+                en_label=self._tags_en_all_list[self._tags_ko_all_list.index(label)]
+                formatted_tag=f'{en_label}({label})'
+                true_label_modified.append(formatted_tag)
+            true_labels.append(true_label_modified)
+        return true_labels
+
